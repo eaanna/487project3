3fcfe2f3f827d9a011c13dcdd6f3a1c278ce8f2c
eaanna <eaanna@live.unc.edu>
2020-10-28 14:28:00 -0400

    added files

--- js/main.js
+++ js/main.js
@@ -1,30 +1,54 @@
 $(document).ready(function() {
     console.log("ready");
+    loadJSON();
 });
 
-function loadData() {
-    $.getJSON("data/youth_ages.json");
-    console.log('json loaded');
+
+var ageGroup = [];
+var males = [];
+var females = [];
+
+function loadJSON() {
+    $.getJSON("youth_ages.json", function (youthages) {
+        console.log(youthages);
+        parseData(youthages);
+    });
+        
+}
+
+function parseData(youthages) {
+
+    var html = "";
+
+    $.each(youthages, function(index) {
+        html += '<h2>' + youthages[index].ageBracket + '<h2>';
+        html += '<ul><li>Males' + youthages[index].males + '</li>';
+        html += '<li>Females:' + youthages[index].females + '</li>';
+        html += '</ul>';
+
+        males.push(youthages[index].males);
+        females.push(youthages[index].females);
+    });
+
+    $("#sport_age_chart").append(html);
     buildCharts();
 }
 
-
 function buildCharts() {
 
-var chart1 = c3.generate({
-    bindto: "#sport_chart",
-    data: {
-        url: 'data/sportsTBI.csv',
-        type: 'bar'
-    }
+    var chart1 = c3.generate({
+        bindto: "#sport_age_chart",
+        data: {
+            url: 'youth_ages.json',
+            mimeType: 'json'
+        },
+        bar: {
+            width: {
+                ratio: 0.5
+            }
+        }
 });
 
-var chart2 = c3.generate({
-    bindto: "#sport_age_chart",
-    data: {
-        url: 'data/youth_ages.json',
-        mimeType: 'json'
-    }
-});
+$("#sport_age_chart").append(chart1);
 
 }
